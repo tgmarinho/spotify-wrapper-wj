0d4da2fd6cc0e2d854987b7e3494709c7c16f63b
Thiago Marinho <tgmarinho@gmail.com>
2018-01-09 12:16:16 -0300

    criando os smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,30 @@
 import { expect } from 'chai';
 
-describe('Main', () => {
+import { search, searchAlbums, searchTracks, searchPlaylists, searchArtists } from '../src/main';
+
+describe('Spotify Wrapper', () => {
+
+    describe('smoke tests', () => {
+     
+        it('should exists the search method', () => {
+            expect(search).to.exist;
+        });
+
+        it('should exists the searchAlbums method', () => {
+            expect(searchAlbums).to.exist;
+        });
+
+        it('should exists the searchArtists method', () => {
+            expect(searchArtists).to.exist;
+        });
+
+        it('should exists the searchTracks method', () => {
+            expect(searchTracks).to.exist;
+        });
+
+        it('should exists the searchPlaylists method', () => {
+            expect(searchPlaylists).to.exist;
+        });
+    });
 
 });
