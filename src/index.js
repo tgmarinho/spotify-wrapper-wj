205002710ed292ed79146fdffaf162493333df00
Thiago Marinho <tgmarinho@gmail.com>
2018-01-11 13:50:51 -0300

    resolvendo erro de digitação e lint

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import {
-    search,
-    searchArtists,
-    searchAlbums,
-    searchPlaylists,
-  } from './search';
-  
-  import {
-    getAlbum,
-    getAlbums,
-    getAlbumTracks,
-  } from './album';
-  
-  module.exports = {
-    search,
-    searchArtists,
-    searchAlbums,
-    searchPlaylists,
-    getAlbum,
-    getAlbums,
-    getAlbumTracks,
-  };
-  
+  search,
+  searchArtists,
+  searchAlbums,
+  searchPlaylists,
+} from './search';
+
+import {
+  getAlbum,
+  getAlbums,
+  getAlbumTracks,
+} from './album';
+
+module.exports = {
+  search,
+  searchArtists,
+  searchAlbums,
+  searchPlaylists,
+  getAlbum,
+  getAlbums,
+  getAlbumTracks,
+};
