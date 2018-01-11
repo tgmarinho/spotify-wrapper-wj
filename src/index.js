4f5f415cc82854650ff42b98363e3d84cb84555c
Thiago Marinho <tgmarinho@gmail.com>
2018-01-11 13:48:40 -0300

    resolvendo problemas

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-/* global mudule */
 import {
-  search,
-  searchArtists,
-  searchAlbums,
-  searchPlaylists,
-} from './search';
-
-import {
-  getAlbum,
-  getAlbums,
-  getAlbumTracks,
-} from './album';
-
-mudule.exports = {
-  search,
-  searchArtists,
-  searchAlbums,
-  searchPlaylists,
-  getAlbum,
-  getAlbums,
-  getAlbumTracks,
-};
+    search,
+    searchArtists,
+    searchAlbums,
+    searchPlaylists,
+  } from './search';
+  
+  import {
+    getAlbum,
+    getAlbums,
+    getAlbumTracks,
+  } from './album';
+  
+  module.exports = {
+    search,
+    searchArtists,
+    searchAlbums,
+    searchPlaylists,
+    getAlbum,
+    getAlbums,
+    getAlbumTracks,
+  };
+  
