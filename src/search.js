6d8c62e7e026a8d598125b6d205080f4b4a11201
Thiago Marinho <tgmarinho@gmail.com>
2018-01-11 11:46:19 -0300

    refatorando e reorganizando o código

--- src/search.js
+++ src/search.js
@@ -1,7 +1,9 @@
 /* global fetch */
+import { API_URL } from './config';
+import { toJSON } from './utils';
+
 export const search = (query, type) =>
-  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
-    .then(data => data.json());
+  fetch(`${API_URL}/search?q=${query}&type=${type}`).then(toJSON);
 
 export const searchArtists = query =>
   search(query, 'artist');
